93f3c6c7f913290402e73f590f41a07d3738cb6d
Brian Pugnali <brian@pugnoni.us>
2026-04-15 13:56:56 -0500

    Reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = "";
+    for (let i = 1; i <= str.length; i++) {
+        newStr += str.at(-1 * i);
+    }
+    return newStr;
 };
 
 // Do not edit below this line
