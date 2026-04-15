a01b43c4f91f095ae0e93946fe9da0c4f9969d6a
Brian Pugnali <brian@pugnoni.us>
2026-04-15 13:18:35 -0500

    Complete js to repeat string using for loop.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = str;
+    if (num > 1) {
+        for (let i = 1; i < num; i++) {
+            newStr += str;
+        }
+    } else if (num < 0) { 
+        return "ERROR"; 
+    } else if (num === 0) {
+        return ""
+    }
 
+    return newStr;
 };
 
 // Do not edit below this line
