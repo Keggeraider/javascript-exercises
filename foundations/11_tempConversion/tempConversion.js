f2d493ebf00ecc29680d2c44661600ee2226b46b
Brian Pugnali <brian@pugnoni.us>
2026-04-15 15:09:22 -0500

    Convert from celsius to farenheit and vise versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if ((temp - 32) / (9/5) === 0) return 0;
+  else return Math.round((temp - 32) / (9/5) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if ((temp * (9/5) + 32) === 0) return 0;
+  else return Math.round((temp * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
