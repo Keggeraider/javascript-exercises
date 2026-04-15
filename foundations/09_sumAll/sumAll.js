d64132ab328c54a63ead97efd4c761c9b1074951
Brian Pugnali <brian@pugnoni.us>
2026-04-15 14:38:19 -0500

    Add function to sum all numbers between two numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (Number.isNaN(a) || Number.isNaN(b)) {
+        return "ERROR";
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+    let sum = 0;
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else if (a > b) {
+        for (let i = b; i <= a; i++){
+            sum += i;
+        }
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
